1c876aa9a917e21d579d995f263872c2b51a2ecb
wangeastsea <wangeastsea@icloud.com>
2020-12-17 15:21:58 +0800

    webpack

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # tiny-webkpack
+https://juejin.cn/post/6844904036710219789
--- dist/main.js
+++ dist/main.js
@@ -10,4 +10,4 @@
                 return exports;
             }
             require('./src/index.js')
-        })({"./src/index.js":{"dependencies":{"./export.js":"./src\\export.js"},"code":"\"use strict\";\n\nvar _export = require(\"./export.js\");\n\n(0, _export.sayHi)(\"hfj\");\nconsole.log(\"hello webpack index\");"},"./src\\export.js":{"dependencies":{},"code":"\"use strict\";\n\nObject.defineProperty(exports, \"__esModule\", {\n  value: true\n});\nexports.sayHi = void 0;\n\nvar sayHi = function sayHi(params) {\n  console.log(\"hello ~\" + params);\n};\n\nexports.sayHi = sayHi;"}})
+        })({"./src/index.js":{"dependencies":{"./export.js":"./src/export.js"},"code":"\"use strict\";\n\nvar _export = require(\"./export.js\");\n\n(0, _export.sayHi)(\"hfj\");\nconsole.log(\"hello webpack index\");"},"./src/export.js":{"dependencies":{},"code":"\"use strict\";\n\nObject.defineProperty(exports, \"__esModule\", {\n  value: true\n});\nexports.sayHi = void 0;\n\nvar sayHi = function sayHi(params) {\n  console.log(\"hello ~\" + params);\n};\n\nexports.sayHi = sayHi;"}})
